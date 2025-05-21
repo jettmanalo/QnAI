63909e544aca5a2283a9d6b74d6caf0d6504d585
jettmanalo <jettmanalo.dev@gmail.com>
2025-05-22 02:23:42 +0800

    Implement interview generation page UI

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,26 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    apiKey: "AIzaSyCdx9uvc1Mjp5Ib6mByiVA-JJ1hb1kuXfw",
+    authDomain: "qnai-950d7.firebaseapp.com",
+    projectId: "qnai-950d7",
+    storageBucket: "qnai-950d7.firebasestorage.app",
+    messagingSenderId: "1083040016000",
+    appId: "1:1083040016000:web:c578d96038c96f0a91e865",
+    measurementId: "G-RCV25QG36V"
 };
 
-// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-// const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
